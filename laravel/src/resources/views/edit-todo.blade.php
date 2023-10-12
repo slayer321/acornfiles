c55a28f8c5b8b080e717485157eae3823aeed0bd
slayer321 <sachin.maurya7666@gmail.com>
2023-10-12 15:17:20 +0530

    fix laravel UI

--- laravel/src/resources/views/edit-todo.blade.php
+++ laravel/src/resources/views/edit-todo.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Edit TODO App!</title>
 </head>
 <body>
 <div class="text-center mt-5">
@@ -59,7 +59,6 @@
         @endif
     </div>
 </div>
-<h1>Hello, world!</h1>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
